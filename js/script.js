254cdbe3321c4b4d0bb0dfac4b3ed82f4820b993
Fareeduddinn <117514172+Fareeduddinn@users.noreply.github.com>
2024-12-31 16:00:46 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let daynight=document.querySelector(".daynight");
+let banner=document.querySelector(".banner");
+daynight.addEventListener("click",()=>
+{
+    banner.classList.toggle("night"); 
+})
+let typingEffect=new Typed("#text",{
+    strings:["Fareed","Web Developer","Coder"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000
+
+})
